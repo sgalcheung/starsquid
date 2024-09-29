816571c85424af3a9ee833a40d04c90b578dea4c
Sgal Cheung <zhangsigao@live.com>
2024-09-29 23:22:34 +0800

    Use Redis to cache the catalog of columns.

---
+++ example/src/lib/redis.ts
@@ -0,0 +1,38 @@
+import { createClient } from "redis";
+
+const client = await createClient({
+  url: import.meta.env.REDIS_URL,
+})
+  .on("error", (err) => console.log("Redis Client Error", err))
+  .connect();
+
+export async function storeRedis(
+  key: string,
+  token: string,
+  expiresInSeconds: number
+) {
+  try {
+    await client.set(key, token, {
+      EX: expiresInSeconds,
+    });
+    console.log("Token stored successfully in Redis.");
+  } catch (error) {
+    console.error("Error storing token in Redis:", error);
+  }
+}
+
+export async function getFromRedis(key: string): Promise<string | null> {
+  try {
+    const token = await client.get(key);
+    if (token) {
+      console.log("Token retrieved successfully from Redis.");
+      return token;
+    } else {
+      console.warn("Token not found in Redis or expired.");
+      return null;
+    }
+  } catch (error) {
+    console.error("Error retrieving token from Redis:", error);
+    return null;
+  }
+}
