8415aa69beff1b544d30a40f0e8b733aee0bac55
Sgal Cheung <zhangsigao@live.com>
2024-11-03 15:34:54 +0800

    Fix bug.

--- example/src/content/config.ts
+++ example/src/content/config.ts
@@ -1,8 +1,15 @@
 import { squidexCollections } from "desquidex/loaders";
+import { docsSchema } from "@astrojs/starlight/schema";
+import { defineCollection } from "astro:content";
 
-export const collections = squidexCollections({
+const defaultCollections = squidexCollections({
   squidexUrl: import.meta.env.SQUIDEX_URL,
   squidexAppName: import.meta.env.SQUIDEX_APP_NAME,
   squidexClientId: import.meta.env.SQUIDEX_CLIENT_ID,
   squidexClientSecret: import.meta.env.SQUIDEX_CLIENT_SECRET,
 });
+// TODO: when support custom default collection, remove this.
+export const collections = {
+  ...defaultCollections,
+  docs: defineCollection({ schema: docsSchema() }),
+};
