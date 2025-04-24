81ea3295b5475b423af5c54c055f556aa397fcfe
Sgal Cheung <zhangsigao@live.com>
2025-04-24 09:49:51 +0800

    refactor: update output paths in ts-to-zod configuration to remove src prefix and add nestedFieldDto

--- packages/desquidex/ts-to-zod.config.cjs
+++ packages/desquidex/ts-to-zod.config.cjs
@@ -7,52 +7,57 @@ module.exports = [
   {
     name: "appDto",
     input: "node_modules/@squidex/squidex/generated/models/AppDto.d.ts",
-    output: "./src/generated/appDto.zod.ts",
+    output: "./generated/appDto.zod.ts",
   },
   {
     name: "resourceLink",
     input: "node_modules/@squidex/squidex/generated/models/ResourceLink.d.ts",
-    output: "./src/generated/resourceLink.zod.ts",
+    output: "./generated/resourceLink.zod.ts",
   },
   {
     name: "featureDto",
     input: "node_modules/@squidex/squidex/generated/models/FeatureDto.d.ts",
-    output: "./src/generated/featureDto.zod.ts",
+    output: "./generated/featureDto.zod.ts",
   },
   {
     name: "featuresDto",
     input: "node_modules/@squidex/squidex/generated/models/FeaturesDto.d.ts",
-    output: "./src/generated/featuresDto.zod.ts",
+    output: "./generated/featuresDto.zod.ts",
   },
   {
     name: "scheduleJobDto",
     input: "node_modules/@squidex/squidex/generated/models/ScheduleJobDto.d.ts",
-    output: "./src/generated/scheduleJobDto.zod.ts",
+    output: "./generated/scheduleJobDto.zod.ts",
   },
   {
     name: "contentDto",
     input: "node_modules/@squidex/squidex/generated/models/ContentDto.d.ts",
-    output: "./src/generated/contentDto.zod.ts",
+    output: "./generated/contentDto.zod.ts",
   },
   {
     name: "statusInfoDto",
     input: "node_modules/@squidex/squidex/generated/models/StatusInfoDto.d.ts",
-    output: "./src/generated/statusInfoDto.zod.ts",
+    output: "./generated/statusInfoDto.zod.ts",
   },
   {
     name: "fieldPropertiesDto",
     input:
       "node_modules/@squidex/squidex/generated/models/FieldPropertiesDto.d.ts",
-    output: "./src/generated/fieldPropertiesDto.zod.ts",
+    output: "./generated/fieldPropertiesDto.zod.ts",
   },
   {
     name: "fieldDto",
     input: "node_modules/@squidex/squidex/generated/models/FieldDto.d.ts",
-    output: "./src/generated/fieldDto.zod.ts",
+    output: "./generated/fieldDto.zod.ts",
+  },
+  {
+    name: "nestedFieldDto",
+    input: "node_modules/@squidex/squidex/generated/models/NestedFieldDto.d.ts",
+    output: "./generated/nestedFieldDto.zod.ts",
   },
   {
     name: "contentsDto",
     input: "node_modules/@squidex/squidex/generated/models/ContentsDto.d.ts",
-    output: "./src/generated/contentsDto.zod.ts",
+    output: "./generated/contentsDto.zod.ts",
   },
 ];
