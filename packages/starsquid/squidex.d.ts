41ef8dd15f00ceb98f680f2bd71c7cbd9f1bc9d4
Sgal Cheung <zhangsigao@live.com>
2025-05-14 21:29:00 +0800

    feat: add squidex module exports for improved integration

---
+++ packages/starsquid/squidex.d.ts
@@ -0,0 +1,13 @@
+import * as mod1 from "@squidex/squidex/wrapper/errors";
+export * from "@squidex/squidex/wrapper/errors";
+export { mod1 as z1 };
+
+import * as mod2 from "@squidex/squidex/generated";
+export * from "@squidex/squidex/generated";
+export { mod2 as z2 };
+
+import { SquidexClients as SquidexClient } from "@squidex/squidex/wrapper/SquidexClient";
+export { SquidexClient } from "@squidex/squidex/wrapper/SquidexClient";
+export { SquidexClient as z3 };
+
+export { mod1, mod2, SquidexClient };
---
+++ packages/starsquid/squidex.mjs
@@ -0,0 +1,7 @@
+import * as mod1 from "@squidex/squidex/wrapper/errors";
+export { mod1 as z1 };
+
+import * as mod2 from "@squidex/squidex/generated";
+export { mod2 as z2 };
+
+export { mod1, mod2 };
