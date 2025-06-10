098ae29884693b3848cf158cbdce2f8e0024697e
Sgal Cheung <zhangsigao@live.com>
2025-06-10 11:04:57 +0800

    chore: remove unused markdown-related dependencies from package.json and pnpm-lock.yaml

--- playground/src/helpers/markdown.ts
+++ playground/src/helpers/markdown.ts
@@ -1,48 +1,48 @@
-import { unified } from "unified";
-import remarkParse from "remark-parse";
-import remarkRehype from "remark-rehype";
-import rehypeStringify from "rehype-stringify";
-import { rehypeHeadingIds } from "@astrojs/markdown-remark";
-import { VFile } from "vfile";
+// import { unified } from "unified";
+// import remarkParse from "remark-parse";
+// import remarkRehype from "remark-rehype";
+// import rehypeStringify from "rehype-stringify";
+// import { rehypeHeadingIds } from "@astrojs/markdown-remark";
+// import { VFile } from "vfile";
 import Slugger from "github-slugger";
 
 import { marked } from "marked";
 import { gfmHeadingId } from "marked-gfm-heading-id";
 
-export async function markdownToHtml(markdown: string): Promise<VFile> {
-	const parser = unified()
-		.use(remarkParse)
-		.use(remarkRehype, {
-			allowDangerousHtml: true,
-			passThrough: [],
-		})
-		.use(rehypeHeadingIds)
-		.use(rehypeStringify, { allowDangerousHtml: true });
+// export async function markdownToHtml(markdown: string): Promise<VFile> {
+// 	const parser = unified()
+// 		.use(remarkParse)
+// 		.use(remarkRehype, {
+// 			allowDangerousHtml: true,
+// 			passThrough: [],
+// 		})
+// 		.use(rehypeHeadingIds)
+// 		.use(rehypeStringify, { allowDangerousHtml: true });
 
-	const vfile = new VFile({
-		value: markdown,
-		// path: `${Astro.url}.mdx`,
-		// data: {
-		//   astro: {
-		//     frontmatter: frontmatter ?? {},
-		//   },
-		// },
-	});
+// 	const vfile = new VFile({
+// 		value: markdown,
+// 		// path: `${Astro.url}.mdx`,
+// 		// data: {
+// 		//   astro: {
+// 		//     frontmatter: frontmatter ?? {},
+// 		//   },
+// 		// },
+// 	});
 
-	let result = undefined;
+// 	let result = undefined;
 
-	try {
-		result = await parser.process(vfile);
-	} catch (err) {
-		// Ensure that the error message contains the input filename
-		// to make it easier for the user to fix the issue
-		// err = prefixError(err, `Failed to parse Markdown file "${vfile.path}"`);
-		console.error(err);
-		throw err;
-	}
+// 	try {
+// 		result = await parser.process(vfile);
+// 	} catch (err) {
+// 		// Ensure that the error message contains the input filename
+// 		// to make it easier for the user to fix the issue
+// 		// err = prefixError(err, `Failed to parse Markdown file "${vfile.path}"`);
+// 		console.error(err);
+// 		throw err;
+// 	}
 
-	return result;
-}
+// 	return result;
+// }
 
 export interface MarkdownHeading {
 	depth: number;
